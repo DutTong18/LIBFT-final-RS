03308834f0ce3b4e169084f138e9d6d18f66380e
dnnphn <97147508+dnnphn@users.noreply.github.com>
2023-03-21 09:53:57 +0000

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -9,5 +9,62 @@
 /*   Updated: 2023/03/15 11:43:42 by dphan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+int	ft_atoi(char *str)
+{
+	int	i;
+	int sign;
+    int res;
+	
+	i = 0;
+	sign = 1;
+    res = 0;
+    while (str[i] != '\0')
+    {   
+        while (str[i] == 32 || (str[i] > 8 && str[i] < 14))
+        {
+            i++;
+        }
 
+        if (str[i] == '-')
+        {
+            sign = sign * -1;
+        }
+        if (str[i] == '+' || str[i] == '-')
+        {
+            i++;
+        }
+        while (str[i] >= '0' && str[i] <= '9')
+        {
+            if (res > 2147483647 / 10 || (res == 2147483647 / 10 && str[i] - '0' > 7))
+            {
+                if (sign == 1)
+                {
+                    res = 2147483647;
+                    return res;
+                }
+                else 
+                {
+                    res = -2147483648;
+                    return res;
+                }
+            }
+            res = res * 10 + (str[i] - '0');
+            i++;
+        }
+        i++;
+    }
+	return (res * sign);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	char word[] = "12323456789098";
+	char *str;
+
+	str = word;
+
+	printf("%d\n", ft_atoi(str));
+	return (0);
+}
 
